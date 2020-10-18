64d58631f3b778efcf48d40a7fb0100eb839b772
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-10-17 20:24:51 -0400

    adding answers

--- Fundamentals/index.002.js
+++ Fundamentals/index.002.js
@@ -7,6 +7,35 @@ No floats or non-positive integers will be passed.
 For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
-
-
 */
+
+// answer 1
+function sumTwoSmallestNumbers(numbers) {
+  // Code here
+  console.log(arguments[0]);
+
+  let randomArray = [];
+  for (let i = 0; i < arguments[0].length; i++) {
+    randomArray.push(arguments[0][i]);
+  }
+
+  let sortArray = randomArray.sort((a, b) => {
+    return a - b;
+  });
+  console.log(sortArray);
+  let sumUp = sortArray[0] + sortArray[1];
+  console.log(Number(sumUp));
+  return Number(sumUp);
+}
+sumTwoSmallestNumbers([1, 20, 30, 2]);
+// answer 2
+
+function sumTwoSmallestNumbers([...numbers]) {
+  // Code here
+
+  let random = numbers.sort((a, b) => {
+    return a - b;
+  });
+  console.log(random[0] + random[1]);
+  return random[0] + random[1];
+}
